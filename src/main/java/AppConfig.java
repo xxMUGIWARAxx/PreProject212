4d0db85f3c2c6951061047576b39e814d8cd567b
MIGIWARA <99168445+MIGIWARA@users.noreply.github.com>
2022-08-25 22:08:01 +0300

    first commit

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
     }
+
     @Bean(name = "cat")
     @Scope(value = "prototype")
     public Cat getCat() {
--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,13 +1,13 @@
 public class HelloWorld {
- 
+
     private String message;
- 
+
     public String getMessage() {
         return message;
     }
- 
+
     public void setMessage(String message) {
         this.message = message;
     }
-     
+
 }
