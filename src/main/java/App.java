9491eb6771c0264487aec9302dbb141b462ef99f
MIGIWARA <99168445+MIGIWARA@users.noreply.github.com>
2022-08-26 08:56:38 +0300

    first commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,12 +6,12 @@ public class App {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+                applicationContext.getBean(HelloWorld.class);
         System.out.println(bean.getMessage());
-        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = applicationContext.getBean(HelloWorld.class);
         System.out.println(bean == bean2);
-        Cat cat = (Cat) applicationContext.getBean("cat");
-        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        Cat cat = applicationContext.getBean(Cat.class);
+        Cat cat2 = applicationContext.getBean(Cat.class);
         System.out.println(cat == cat2);
     }
 }
